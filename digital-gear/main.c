9a395adb7dfa44f882dfa528d9a69e48079d9086
AyaAbrada <abradaaya2@gmail.com>
2024-10-31 14:17:01 +0100

    filtrage des taches

--- digital-gear/main.c
+++ digital-gear/main.c
@@ -15,7 +15,7 @@ Tache taches[MAX_TACHES];
 int nombreTaches = 0;
 
 void afficherMenu() {
-    printf("\n--- Menu ---\n");
+    printf("\n---{ Menu }---\n");
     printf("1. Ajouter une Tache\n");
     printf("2. Afficher les Taches\n");
     printf("3. Modifier une Tache\n");
@@ -41,5 +41,117 @@ void ajouterTache() {
     scanf(" %[^\n]", nouvelleTache.priorite);
 
     taches[nombreTaches++] = nouvelleTache;
-    printf("Tache ajoutee avec succes !\n");
+    printf("Tache ajoutae avec succes !\n");
+}
+
+void afficherTaches() {
+    if (nombreTaches == 0) {
+        printf("Aucune tache a afficher.\n");
+        return;
+    }
+
+    for (int i = 0; i < nombreTaches; i++) {
+        printf("Tache %d :\n", i + 1);
+        printf("Titre : %s\n", taches[i].titre);
+        printf("Description : %s\n", taches[i].description);
+        printf("Date d'echeance : %s\n", taches[i].dateEcheance);
+        printf("Priorite : %s\n", taches[i].priorite);
+        printf("----------------------\n");
+    }
+}
+
+void modifierTache() {
+    int index;
+    printf("Entrez le numero de la tache a modifier (1 a %d) : ", nombreTaches);
+    scanf("%d", &index);
+
+    if (index < 1 || index > nombreTaches) {
+        printf("Erreur : Numero de tache invalide.\n");
+        return;
+    }
+
+    Tache *tache = &taches[index - 1];
+    printf("Modifier la description (actuelle : %s) : ", tache->description);
+    scanf(" %[^\n]", tache->description);
+    printf("Modifier la date d'echeance (actuelle : %s) : ", tache->dateEcheance);
+    scanf(" %[^\n]", tache->dateEcheance);
+    printf("Modifier la priorite (actuelle : %s) : ", tache->priorite);
+    scanf(" %[^\n]", tache->priorite);
+
+    printf("Tache modifiee avec succes !\n");
+}
+
+void supprimerTache() {
+    int index;
+    printf("Entrez le numero de la tache a supprimer (1 à %d) : ", nombreTaches);
+    scanf("%d", &index);
+
+    if (index < 1 || index > nombreTaches) {
+        printf("Erreur : Numero de tache invalide.\n");
+        return;
+    }
+
+    for (int i = index - 1; i < nombreTaches - 1; i++) {
+        taches[i] = taches[i + 1];
+    }
+    nombreTaches--;
+    printf("Tache supprimee avec succes !\n");
+}
+
+void filtrerTaches() {
+    char priorite[10];
+    printf("Entrez la priorite a filtrer (High/Low) : ");
+    scanf(" %[^\n]", priorite);
+
+    int trouve = 0;
+    for (int i = 0; i < nombreTaches; i++) {
+        if (strcmp(taches[i].priorite, priorite) == 0) {
+            printf("Tache %d :\n", i + 1);
+            printf("Titre : %s\n", taches[i].titre);
+            printf("Description : %s\n", taches[i].description);
+            printf("Date d'echeance : %s\n", taches[i].dateEcheance);
+            printf("Priorite : %s\n", taches[i].priorite);
+            printf("----------------------\n");
+            trouve = 1;
+        }
+    }
+
+    if (!trouve) {
+        printf("Aucune tache trouvee avec la priorite '%s'.\n", priorite);
+    }
+}
+
+int main() {
+    int choix;
+
+    do {
+        afficherMenu();
+        printf("Entrez votre choix : ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1:
+                ajouterTache();
+                break;
+            case 2:
+                afficherTaches();
+                break;
+            case 3:
+                modifierTache();
+                break;
+            case 4:
+                supprimerTache();
+                break;
+            case 5:
+                filtrerTaches();
+                break;
+            case 0:
+                printf("Au revoir !\n");
+                break;
+            default:
+                printf("Choix invalide. Veuillez réessayer.\n");
+        }
+    } while (choix != 0);
+
+    return 0;
 }
